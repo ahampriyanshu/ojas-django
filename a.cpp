a1c1beabfb60cd6d8b56614c272b530ec7e7e38f
priyanshumay <tiwarimay2002@gmail.com>
2020-11-23 23:10:57 +0530

    upgrading UI : IX

---
+++ a.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    return 0;
+}
