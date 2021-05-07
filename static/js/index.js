2e784fc78e779bfaf7ecff16b6587b272938e261
ahampriyanshu <ahampriyanshu@gmail.com>
2021-05-07 18:12:15 +0530

    applying dark mode III , updated 8bit favicon and converted offline page to pure css

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,9 @@
 $(document).ready(function () {
-    preloaderFadeOutTime = 1000;
+    preloaderFadeOutTime = 100;
 
     function hidePreloader() {
         var preloader = $('.spinner-wrapper');
         preloader.fadeOut(preloaderFadeOutTime);
     }
     hidePreloader();
-});
-
-let searchText = [
-    "How to sign raw bitcoin transaction",
-    "Using trezor-firmware",
-    "Switching from bootstrap to tailwind",
-    "How to sign raw ethereum transaction",
-    "Understanding Shamir Secret Sharing algorithm",
-];
-
-var generate = document.getElementById('searchBar');
-generate.value = searchText[Math.floor(Math.random()*searchText.length)];
+}); 
