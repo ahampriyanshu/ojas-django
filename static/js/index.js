9b60e94094f8513403615a670d0ac4b1ec45a8a8
ahampriyanshu <ahampriyanshu@gmail.com>
2021-05-10 04:06:01 +0530

    added preloader and updated darkMode retention via localstorage

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-    preloaderFadeOutTime = 100;
-
+    preloaderFadeOutTime = 500;
     function hidePreloader() {
-        var preloader = $('.spinner-wrapper');
+        var preloader = $('.preloader');
         preloader.fadeOut(preloaderFadeOutTime);
     }
     hidePreloader();
@@ -13,4 +12,13 @@ $('.captcha').click(function () {
         $('.captcha').attr('src', result['image_url']);
         $('#id_captcha_0').val(result['key'])
     });
+});
+
+const toggle = document.querySelector('.toggleSwitch');
+toggle.addEventListener('click', () => {
+    if (localStorage.darkMode === true) {
+        localStorage.darkMode = false
+    } else {
+        localStorage.darkMode = true
+    }
 });
